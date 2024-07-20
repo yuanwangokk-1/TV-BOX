1c770ef7e0689909c1182483292c52a3f1119a4a
yuanwangokk-1 <yuanwangokk@proton.me>
2024-07-20 22:26:58 +0800

    main

--- drpy_dzlive/drpy_js/影视/时光影院.js
+++
@@ -1,7 +0,0 @@
-var rule = {
-    模板: 'mxone5',
-    title: '时光影院',
-    host: 'https://www.time-chicken.com',
-    url: '/vod/show/id/fyclass/page/fypage.html',
-    searchUrl: '/vod/search/page/fypage/wd/**.html',
-}
--- drpy_dzlive/drpy_js/影视/茶语资源[资].js
+++
@@ -1,5 +0,0 @@
-var rule = {
-    模板: '采集1',
-    title: '茶语资源[资]',
-    host: 'http://www.clcaiji.com:1001',
-}
